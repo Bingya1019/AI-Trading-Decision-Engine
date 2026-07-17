a7793527963b9a1edbec4086417430b9ff0e1cb1
BingYa <bingya2018615@gmail.com>
2026-07-17 21:28:32 +0800

    V3.4 BOS V1 完成

--- core/pipeline.py
+++ core/pipeline.py
@@ -4,7 +4,7 @@ from analysis.emotion import analyze_emotion
 from analysis.volatility import analyze_volatility
 from analysis.volume import analyze_volume
 from analysis.structure import analyze_structure
-
+from analysis.bos import analyze_bos
 
 def run_pipeline(data):
     current = data.iloc[-1]
@@ -12,17 +12,18 @@ def run_pipeline(data):
     price = current["close"]
 
     score_items = [
-        analyze_trend(price, current["ema20"], current["ema60"]),
-        analyze_momentum(current["macd_hist"], previous["macd_hist"]),
-        analyze_emotion(current["rsi14"]),
-        analyze_volatility(
-            price,
-            current["boll_high"],
-            current["boll_mid"],
-            current["boll_low"],
-        ),
-        analyze_volume(data),
-        analyze_structure(data),
-    ]
+    analyze_trend(price, current["ema20"], current["ema60"]),
+    analyze_momentum(current["macd_hist"], previous["macd_hist"]),
+    analyze_emotion(current["rsi14"]),
+    analyze_volatility(
+        price,
+        current["boll_high"],
+        current["boll_mid"],
+        current["boll_low"],
+    ),
+    analyze_volume(data),
+    analyze_structure(data),
+    analyze_bos(data),
+]
 
     return score_items
